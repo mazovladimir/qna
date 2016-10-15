c8a43ed0fa7a2801cb4e65266e4833ffd9134973
root <root@kvm.lab.com>
2016-10-15 22:19:04 +0300

    Added dependent destroy to model

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,5 @@ class Question < ActiveRecord::Base
   validates :body, presence: true, length: { minimum: 2 }
   validates :title, presence: true, length: { in: 10..200 }
 
-  has_many :answers
+  has_many :answers, dependent: :destroy
 end
