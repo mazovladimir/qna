8dc4418c5a6fe5b9e9088a1d6397fa4b06ad8e1b
root <root@kvm.lab.com>
2016-10-15 23:14:11 +0300

    Added Validations

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { should validate_presence_of :title}
   it { should validate_presence_of :body}
+  it { should validate_length_of(:body).is_at_least(2) }
+  it { should validate_length_of(:title).is_at_least(10).is_at_most(200) }
+  it { should have_many(:answers).dependent(:destroy) }
 end
