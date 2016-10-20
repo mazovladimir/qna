25ffe1daae2feba43aed8ca5f37d2ff6c86c64c2
root <root@kvm.lab.com>
2016-10-20 19:10:53 +0300

    Error redirect

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,10 +16,10 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = Question.create(question_params)
+    @question = Question.new(question_params)
 
     if @question.save
-      redirect_to @question
+      redirect_to question_path
     else
       render :new
     end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -60,7 +60,7 @@ RSpec.describe QuestionsController, type: :controller do
 
       it 'redirects to show view' do
         post :create, params: { question: attributes_for(:question) }
-        expect(response).to redirect_to question_path(assigns(:question))
+        expect(response).to redirect_to(assigns(:question))
       end
     end
 
