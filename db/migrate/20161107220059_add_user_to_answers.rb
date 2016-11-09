de73db6f000bcb153920c1057041f4c2402d7324
root <root@kvm.lab.com>
2016-11-09 13:22:35 +0300

    Resolved association foreign key

--- db/migrate/20161107220059_add_user_to_answers.rb
+++ db/migrate/20161107220059_add_user_to_answers.rb
@@ -1,5 +1,5 @@
 class AddUserToAnswers < ActiveRecord::Migration[5.0]
   def change
-    add_column :answers, :user_id, :integer
+    add_belongs_to :answers, :user, index: true, foreign_key: true 
   end
 end
