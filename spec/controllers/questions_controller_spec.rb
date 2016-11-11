228899cd5b216457b2daba40933857d28ea8cfff
root <root@kvm.lab.com>
2016-11-11 20:16:46 +0300

    removed edit from spec as not needed

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -43,20 +43,6 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
-  describe 'GET #edit' do
-    sign_in_user
-
-    before { get :edit, params: { id: question } }
-
-    it 'assigns a requested question to @question' do
-      expect(assigns(:question)).to eq question
-    end
-
-    it 'renders edit view' do
-      expect(response).to render_template :edit
-    end
-  end
-
   describe 'POST #create' do
     sign_in_user
 
