5b001a3d5791a6070693bcbea3c0196b636fdae7
root <root@kvm.lab.com>
2016-11-13 21:15:26 +0300

    Added acceptance answer delete non-auth 2

--- spec/acceptance/delete_question_spec.rb
+++ spec/acceptance/delete_question_spec.rb
@@ -12,25 +12,19 @@ feature 'Delete question', %q{
 
   scenario 'Authenticated user deletes question' do
     sign_in(user)
-
     visit questions_path
-
     click_on 'Delete question'
-
     expect(page).to_not have_content 'More than 10 symbols'
   end
 
   scenario 'Non-Author Authenticated user deletes question' do
     sign_in(user2)
-
     visit questions_path
-
     expect(page).to_not have_content 'Delete question'
   end  
 
   scenario 'Non-Authenticated user deletes question' do
     visit questions_path
-
     expect(page).to_not have_content 'Delete question'
   end
 end
