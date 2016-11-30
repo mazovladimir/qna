5b31a668b5b35f1ef350e1dd332538588dfe7257
root <root@kvm.lab.com>
2016-11-30 23:21:01 +0300

    best answer in model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,11 @@ class Answer < ActiveRecord::Base
   belongs_to :user
 
   validates :body, presence: true, length: { minimum: 10 }
+
+  def best_answer
+    self.update(best: 1)
+    Answer.all.each do |myanswer|
+      myanswer.update(best: 0) if myanswer.id != self.id
+    end
+  end
 end
