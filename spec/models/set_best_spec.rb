c45cbcec2b463333e7b66b384b8b88bb082e24f4
root <root@kvm.lab.com>
2016-12-14 12:41:11 +0300

    answer rspec ready 3

--- spec/models/set_best_spec.rb
+++ spec/models/set_best_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   let(:user) { create(:user, email: 'vovka@ruzik.net', password: '12345678', password_confirmation: '12345678') }
   let(:question) { create(:question, user: user) }
-  let!(:answer1) { create(:answer, question: question, user: user) }
-  let!(:answer2) { create(:answer, question: question, user: user) }
+  let(:answer1) { create(:answer, question: question, user: user) }
+  let(:answer2) { create(:answer, question: question, user: user) }
 
   it 'should check best answer' do
     expect(answer1.best).to eq false
